596524490eb3d5e36225977f73da085d971f0b01
coozhangyb <13738043225@163.com>
2014-07-02 23:41:17 +0800

    Update FileUtils.java

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,11 +1,26 @@
 package utils;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.File;
+import java.io.FileFilter;
 
 /**
  * Created by zhangyb on 14-6-30.
  */
 public class FileUtils {
+    /**
+	 * 返回目录下的所有文件(不包括文件夹)
+	 * @param dir
+	 * @return
+	 */
+	private static File[] dirList(String dir) {
+		File path = new File(dir);
+		File[] files = path.listFiles(new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				return pathname.isFile();
+			}
+		});
+		return files;
+	}
     
 }
